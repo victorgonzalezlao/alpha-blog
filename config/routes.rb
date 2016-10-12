ec0c5bbfc9c07668474cfcf6daf112c33fc38722
Victor Gonzalez Lao <victor.gonzalez.lao@gmail.com>
2016-10-12 23:28:17 +0100

    create new template, action aand route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles #this is equivalent to create get, patch, put routes, it creates all of them at the same time. Check at rake routes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
