366e705947198316a8fea22f75d6ed5a53665fb9
Victor Gonzalez Lao <victor.gonzalez.lao@gmail.com>
2016-10-14 18:26:24 +0100

    added destroy and partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,13 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end 
   
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully destryed"
+    redirect_to articles_path
+  end
+  
   def new
     @article = Article.new
   end
