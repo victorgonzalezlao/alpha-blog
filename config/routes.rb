103d97ee28876716badd5a92780c1df6aa2200c0
Victor Gonzalez Lao <victor.gonzalez.lao@gmail.com>
2016-10-27 22:44:37 +0100

    log in first step

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'signup', to: 'users#new'
   resources :users, except: [:new] #this creates all the routes for users except the new which is created above
-
+  get 'login', to: 'sessions#new' #sessions will be a new controller and new will be the action
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy' 
 end
